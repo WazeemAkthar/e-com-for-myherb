12fac25174c7053132f998bb96bf9e65a0e475be
Wazeem Akthar <aktharwazeem@gmail.com>
2025-03-24 16:27:39 +0530

    bug fixed and updated PRODUCTS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/redux/provider';
+import { Analytics } from "@vercel/analytics/react"
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +24,7 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <Analytics/>
       </body>
     </html>
   );
